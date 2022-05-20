55081d7e5046a072325e534a74e4770cef1929a3
HodayaTakele <hodayat@post.bgu.ac.il>
2022-05-20 21:01:16 +0300

    reset forms fixed

--- JS/welcome.js
+++ JS/welcome.js
@@ -2,10 +2,12 @@
 
 function welcomeToSignUp() {
     $("#welcome_page").hide();
+    signup_form.reset();
     $("#signup_page").show();
 }
 
 function welcomeToLogIn() {
     $("#welcome_page").hide();
+    login_form.reset();
     $("#login_page").show();
 }
